7b58ec3c6a172ab5468aa4cfed20e49470536322
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-04-14 18:01:04 +0200

    ADD snack 8 funct+callback AND arrow

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,17 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+function findClass (student){
+  let classe
+  if (student.name === 'Marco Lanci') {
+    classe = student.class
+  }
+  return classe;
+}
+const lanciClass = students.find(findClass).class;
+console.log(lanciClass);
+
+
+const frecciaClass = students.find(student => student.name === 'Marco Lanci').class;
+console.log(frecciaClass);
