13f26962278191548d215bd786d8ffd07a6663a0
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-04-14 15:40:49 +0200

    ADD snack 1 f+callback, f anonima

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,15 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+// Funzione con callback separato definito esternamente
+function printNames (nome, index) {
+    console.log((nome));
+}
+names.forEach(printNames);
+
+// Funzione anonima
+names.forEach(function (nome, i) {
+    console.log(nome);
+});
+
